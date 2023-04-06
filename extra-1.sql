f99f7641d5efbd671a13bc77c6b36d193d72051b
katie1616 <khulst@uw.edu>
2023-04-06 07:53:07 -0700

    extra credit

---
+++ extra-1.sql
@@ -0,0 +1,1 @@
+select title, age from persons where gender = 'Female' and title = 'Dr' and city = 'West Palm Beach' and state = 'Florida' OR gender = 'Male' and title = 'Rev' and city = 'Phoenix' and state = 'Arizona';
